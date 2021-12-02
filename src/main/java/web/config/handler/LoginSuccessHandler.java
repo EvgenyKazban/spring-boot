0becf72381143fae4ab3b9b3c7a3323be7f47448
Evgeniy Kazban <evgeny.kazban3.0@yandex.ru>
2021-12-03 00:43:23 +0300

    roles based auth success redirect

--- src/main/java/web/config/handler/LoginSuccessHandler.java
+++ src/main/java/web/config/handler/LoginSuccessHandler.java
@@ -3,11 +3,13 @@ package web.config.handler;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import web.model.Role;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @Component
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
@@ -16,6 +18,15 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest,
                                         HttpServletResponse httpServletResponse,
                                         Authentication authentication) throws IOException, ServletException {
-        httpServletResponse.sendRedirect(httpServletRequest.getContextPath() + "/user");
+
+        List<Role> roles = (List<Role>) authentication.getAuthorities();
+
+        boolean hasAdmin = roles.stream().anyMatch(r -> r.getRoleName().equals("ROLE_ADMIN"));
+
+        if(hasAdmin) {
+            httpServletResponse.sendRedirect(httpServletRequest.getContextPath() + "/admin/users");
+        } else {
+            httpServletResponse.sendRedirect(httpServletRequest.getContextPath() + "/user");
+        }
     }
 }
